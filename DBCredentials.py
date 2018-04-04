4af94b04dcb987a95d57e35de6fa464f0a8c0871
pleluron <pierre.leluron@gmail.com>
2018-04-04 18:15:26 +0200

    Cleanup

---
+++ DBCredentials.py
@@ -0,0 +1,13 @@
+irmaDB_v1 = {
+	"user":"typer",
+	"passwd":"i31YyYerEZ9wbLan",
+	"host":"148.60.11.195",
+	"db": "IrmaDB",
+}
+
+irmaDB_v2 = {
+    "user":"typer",
+    "passwd":"i31YyYerEZ9wbLan",
+    "host":"148.60.11.195",
+    "db": "IrmaDB_prod",
+}
