dee473aca71a30f4e58aff3bb8d414942b60410b
pleluron <pierre.leluron@gmail.com>
2018-04-30 18:53:46 +0200

    All working now

--- filltypes.py
+++ filltypes.py
@@ -8,7 +8,8 @@ def fill_types(input):
     kconf = Kconfig(input)
 
     # SQL requests
-    fill = "TRUNCATE TABLE Properties; INSERT INTO Properties (name, type) VALUES (%s, %s)"
+    truncate = "TRUNCATE TABLE Properties"
+    fill = "INSERT INTO Properties (name, type) VALUES (%s, %s)"
 
     if len(DBCredentials.db) == 0:
         print("No database(s) have been selected. Please modify DBCredentials.py to add your databases")
@@ -27,6 +28,7 @@ def fill_types(input):
                     TYPE_TO_STR[sym.orig_type].upper()))
 
             # Remove previous entries and add properties
+            cursor.execute(truncate)
             cursor.executemany(fill, data_fill)
             conn.commit()
 
